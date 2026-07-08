06fa3f0a0fda69e6f30a2d3d9624f53a748b20d4
OctoFlare <62318879+OctoFlareDev@users.noreply.github.com>
2026-07-08 10:34:29 +0800

    Initial Commit

---
+++ Musicalculator/MusicalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicalculatorApp.swift
+//  Musicalculator
+//
+//  Created by Flare ​ on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicalculatorTests/MusicalculatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  MusicalculatorTests.swift
+//  MusicalculatorTests
+//
+//  Created by Flare ​ on 7/8/26.
+//
+
+import Testing
+@testable import Musicalculator
+
+struct MusicalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
